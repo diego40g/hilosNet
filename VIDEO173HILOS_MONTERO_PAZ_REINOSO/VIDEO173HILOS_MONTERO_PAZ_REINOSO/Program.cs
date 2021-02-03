26cd81cf6efacf2ad2d698732931169795f36913
diego40g <diegopaznaula@hotmail.com>
2021-02-03 15:22:15 -0500

    implementación de funciones

--- VIDEO173HILOS_MONTERO_PAZ_REINOSO/VIDEO173HILOS_MONTERO_PAZ_REINOSO/Program.cs
+++ VIDEO173HILOS_MONTERO_PAZ_REINOSO/VIDEO173HILOS_MONTERO_PAZ_REINOSO/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace VIDEO173HILOS_MONTERO_PAZ_REINOSO
@@ -16,6 +16,43 @@ namespace VIDEO173HILOS_MONTERO_PAZ_REINOSO
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+        }
+    }
+
+    class Banco
+    {
+        private double[] cuentas;
+
+        public Banco()
+        {
+            cuentas = new double[100];
+            for (int i = 0; i < cuentas.Length; i++)
+            {
+                cuentas[i] = 2000;
+            }
+        }
+
+        public void transferencia (int cuentaOrigen, int cuentaDestino, double cantidad)
+        {
+            if (cuentas[cuentaOrigen] < cantidad)//evalúa q el saldo no es inferior a la transferencia
+            {
+                return;
+            }
+            Console.WriteLine(Thread.CurrentThread.ToString());
+            cuentas[cuentaOrigen] -= cantidad;//dinero q sale de la cuenta origen
+            Console.WriteLine(cantidad + " de " + cuentaOrigen + " para " + cuentaDestino);
+            cuentas[cuentaDestino] += cantidad;
+            Console.WriteLine("Saldo total: " + getSaldoTotal());
+        }
+
+        public double getSaldoTotal()
+        {
+            double suma_cuentas = 0;
+            foreach (var a in cuentas)
+            {
+                suma_cuentas += a;
+            }
+            return suma_cuentas;
         }
     }
 }
