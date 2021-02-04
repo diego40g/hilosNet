ee3f84e94782e38cb04d565b70aa4baf336ec291
diego40g <diegopaznaula@hotmail.com>
2021-02-03 23:31:51 -0500

    Solo coloca imprimir en el condicional para que el hilo no tenga valores negativos si no exitiera el return en el IF

---
+++ VIDEO176HILOS_MONTERO_PAZ_REINOSO/VIDEO176HILOS_MONTERO_PAZ_REINOSO/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace VIDEO176HILOS_MONTERO_PAZ_REINOSO
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Banco b = new Banco();
+            //b.cuentasImp();
+            for (int i = 0; i < 100; i++)
+            {
+                EjecucionTransferencia r = new EjecucionTransferencia(b, i, 2000);
+                Thread t = new Thread(r.run);
+                t.Start();
+            }
+        }
+    }
+    class Banco
+    {
+        private double[] cuentas { get; set; }
+        private static ReaderWriterLock rwl = new ReaderWriterLock();
+
+        public void cuentasImp()
+        {
+            for (int i = 0; i < cuentas.Length; i++)
+            {
+                Console.WriteLine(cuentas[i]);
+            }
+        }
+        public Banco()
+        {
+            cuentas = new double[100];
+            for (int i = 0; i < cuentas.Length; i++)
+            {
+                cuentas[i] = 2000;
+            }
+        }
+
+        public void transferencia(int cuentaOrigen, int cuentaDestino, double cantidad)
+        {
+            rwl.AcquireWriterLock(-1);
+            try
+            {
+                if (cuentas[cuentaOrigen] < cantidad)//evalúa q el saldo no es inferior a la transferencia
+                {
+                    Console.WriteLine("-------------------CANTIDAD INSUFICIENTE" + cuentaOrigen + "......SALDO: " + cuentas[cuentaOrigen] + "...." + cantidad);
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("----------CANTIDAD OK----------" + cuentaOrigen);
+                }
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                cuentas[cuentaOrigen] -= cantidad;//dinero q sale de la cuenta origen
+                Console.WriteLine(cantidad + " de " + cuentaOrigen + " para " + cuentaDestino);
+                cuentas[cuentaDestino] += cantidad;
+                Console.WriteLine("Saldo total: " + getSaldoTotal());
+            }
+            finally
+            {
+                rwl.ReleaseLock();
+            }
+        }
+
+        public double getSaldoTotal()
+        {
+            double suma_cuentas = 0;
+            foreach (var a in cuentas)
+            {
+                suma_cuentas += a;
+            }
+            return suma_cuentas;
+        }
+    }
+    class EjecucionTransferencia
+    {
+        private Banco banco;
+        private int deLaCuenta;
+        private double cantidadMax;
+        public EjecucionTransferencia(Banco b, int de, double max)
+        {
+            banco = b;
+            deLaCuenta = de;
+            cantidadMax = max;
+        }
+        public void run()
+        {
+            var rand = new Random();
+            try
+            {
+                while (true)
+                {
+                    int paraLaCuenta = rand.Next(100);
+                    double cantidad = cantidadMax * rand.NextDouble();
+                    banco.transferencia(deLaCuenta, paraLaCuenta, cantidad);
+                    Thread.Sleep(rand.Next(100));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+        }
+    }
+}
